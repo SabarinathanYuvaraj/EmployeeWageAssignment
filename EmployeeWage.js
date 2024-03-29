f0a8356e16793843652f5608ce8d3dde57343409
SabarinathanYuvaraj <sabarinathanyuvaraj321@gmail.com>
2024-03-29 11:12:28 +0530

    [sabarinathan] Add : generate attendance of the employee

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,1 +1,14 @@
 console.log("Welcome to EmployeeWage Computation Program");
+
+function employeeAttendence(){
+    let attendence = Math.floor(Math.random() * 2)
+    if(attendence == 0){
+        console.log("\nthe employee is absent\n");
+    }
+    else{
+        console.log("\nthe employee is present\n");
+
+    }
+}
+
+employeeAttendence()
