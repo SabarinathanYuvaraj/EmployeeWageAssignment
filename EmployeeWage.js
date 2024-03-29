ff2f08ddb42694de2baf806e359d343a4ed13848
SabarinathanYuvaraj <sabarinathanyuvaraj321@gmail.com>
2024-03-29 11:38:55 +0530

    [sabarinathan] Add : generate part wage of employee

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -2,28 +2,44 @@ console.log("Welcome to EmployeeWage Computation Program");
 
 const wagePerHour = 20
 const fullTimeHour = 8
+const partTimeHour  = 4
 let employeeStatus = ""
+const fullTimeWage =  fullTimeHour * wagePerHour 
+const partTimeWage =  partTimeHour * wagePerHour
+
+
 
 let employeeAttendence = () =>{
-    let attendence = Math.floor(Math.random() * 2)
-    if(attendence == 0){
+    let attendence = Math.floor(Math.random() * 3)
+   
+    if(attendence == 1){
+        employeeStatus = "fullTime"
+        console.log("\nthe employee is full-timely present\n");
+
+    }
+
+    else if(attendence == 2){
+        employeeStatus = "partTime"
+        console.log("\nthe employee is  part-timely present\n");
+    }
+
+     else{
         employeeStatus = "absent"
         console.log("\nthe employee is absent\n");
     }
-    else{
-        employeeStatus = "present"
-        console.log("\nthe employee is present\n");
-
-    }
+  
 }
 
 
 let employeeDailyWage = () => {
-if(employeeStatus == "present"){
-let dailyWage =  fullTimeHour * wagePerHour 
-return `the daily wage for employee : ${dailyWage}\n`
+if(employeeStatus == "fullTime"){ 
+return `the daily wage for full time employee : ${fullTimeWage}\n`
 }
-else{
+else if(employeeStatus == "partTime"){
+
+    return `the daily wage for part time employee : ${partTimeWage}\n`
+}
+else {
     return `the employee is absent ,so he can't get wage today\n`
 }
 }
